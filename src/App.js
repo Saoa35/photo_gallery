49a91de78fbfeea0e14f31265139575ea1422317
Saoa35 <saoamah@gmail.com>
2022-10-07 19:39:55 +0200

    import Collection comp. to App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
 
 import React from 'react';
+import  { Collection }  from "./Collection.js";
 import './index.scss';
 
-
-function Collection({ name, images }) {
-
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
 
 function App() {
   
