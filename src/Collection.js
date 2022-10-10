f8339921e936b0a446fa8bf5c925178d31bc144e
Saoa35 <saoamah@gmail.com>
2022-10-10 22:05:02 +0200

    + onClick method to img

--- src/Collection.js
+++ src/Collection.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 export function Collection({ name, images }) {
 
+  const [isActive, setIsActive] = useState(false);
+
     return (
       <div className="collection">
-        <img className="collection__big" src={images[0]} alt="Item" />
+        <img onClick={(e) => e.target === isActive ? setIsActive(true) : setIsActive(false)} className={!isActive ? "collection__big" : "collection__mini"} src={!isActive ? images[0] : images.map((el, i) => [el[0], el[i]] = [el[i], el[0]])} alt="Item" />
         <div className="collection__bottom">
-          <img className="collection__mini" src={images[1]} alt="Item" />
-          <img className="collection__mini" src={images[2]} alt="Item" />
-          <img className="collection__mini" src={images[3]} alt="Item" />
+          <img onClick={(e) => e.target !== isActive ? setIsActive(true) : setIsActive(false)} className={isActive ? "collection__big" : "collection__mini"} src={isActive ? images[1] : images.map((el, i) => [el[1], el[i]] = [el[i], el[1]])} alt="Item"/>
+          <img onClick={(e) => e.target !== isActive ? setIsActive(true) : setIsActive(false)} className={isActive ? "collection__big" : "collection__mini"} src={isActive ? images[2] : images.map((el, i) => [el[2], el[i]] = [el[i], el[2]])} alt="Item"/>
+          <img onClick={(e) => e.target !== isActive ? setIsActive(true) : setIsActive(false)} className={isActive ? "collection__big" : "collection__mini"} src={isActive ? images[3] : images.map((el, i) => [el[3], el[i]] = [el[i], el[3]])} alt="Item" />
         </div>
         <h4>{name}</h4>
       </div>
