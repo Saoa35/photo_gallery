be81d2c3eef125b496d57dc4132f4bbe806b999b
Saoa35 <saoamah@gmail.com>
2022-10-11 09:57:10 +0200

    changed setActive in onClick method

--- src/Collection.js
+++ src/Collection.js
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 
 export function Collection({ name, images }) {
 
-  const [isActive, setIsActive] = useState(null);
+  const [isActive, setIsActive] = useState([0]);
 
     return (
       <div className="collection">      
         <img className={"collection__big"} src={ images[0]  } alt="Item" />    
         <div className="collection__bottom">
           <img 
-            onClick={(e) => setIsActive(e.target)} 
+            onClick={() => setIsActive([images[0], images[1]] = [images[1], images[0]])} 
             className={"collection__mini"} 
-            src={isActive ? [images[0], images[1]] = [images[1], images[0]] : images[1]} 
+            src={images[1]} 
             alt="Item"/>
           <img  
-            onClick={() => setIsActive(true)}
+             onClick={() => setIsActive([images[0], images[2]] = [images[2], images[0]])}
             className={"collection__mini"} 
             src={images[2]} 
             alt="Item"/>
-          <img  
+          <img 
+            onClick={() => setIsActive([images[0], images[3]] = [images[3], images[0]])} 
             className={"collection__mini"} 
             src={images[3]} 
             alt="Item" />
