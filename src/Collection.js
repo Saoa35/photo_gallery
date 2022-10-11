ece4b16f1a40f92453482145627ec316d9c1f0dd
Saoa35 <saoamah@gmail.com>
2022-10-11 10:10:15 +0200

    set isActive state

--- src/Collection.js
+++ src/Collection.js
@@ -3,24 +3,24 @@ import React, { useState } from 'react';
 
 export function Collection({ name, images }) {
 
-  const [isActive, setIsActive] = useState([0]);
+  const [isActive, setIsActive] = useState(images);
 
     return (
       <div className="collection">      
-        <img className={"collection__big"} src={ images[0]  } alt="Item" />    
+        <img className={"collection__big"} src={ isActive[0]  } alt="Item" />    
         <div className="collection__bottom">
           <img 
-            onClick={() => setIsActive([images[0], images[1]] = [images[1], images[0]])} 
+            onClick={() => setIsActive([isActive[0], isActive[1]] = [isActive[1], isActive[0]])} 
             className={"collection__mini"} 
             src={images[1]} 
             alt="Item"/>
           <img  
-             onClick={() => setIsActive([images[0], images[2]] = [images[2], images[0]])}
+             onClick={() => setIsActive([isActive[0], isActive[2]] = [isActive[2], isActive[0]])}
             className={"collection__mini"} 
             src={images[2]} 
             alt="Item"/>
           <img 
-            onClick={() => setIsActive([images[0], images[3]] = [images[3], images[0]])} 
+            onClick={() => setIsActive([isActive[0], isActive[3]] = [isActive[3], isActive[0]])} 
             className={"collection__mini"} 
             src={images[3]} 
             alt="Item" />
