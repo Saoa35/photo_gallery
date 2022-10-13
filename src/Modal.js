dd39b7e672bfc5aa6292ae147333151a3a89203d
Saoa35 <saoamah@gmail.com>
2022-10-13 21:22:20 +0200

    + modal styles

--- src/Modal.js
+++ src/Modal.js
@@ -1,52 +1,16 @@
-import React, { useState } from 'react';
-import  ReactDOM  from 'react-dom';
+import React from 'react';
 
 
-export function Modal({ children }) {
-
-const modal = document.getElementById('modal');
-  
-  return ReactDOM.createPortal (
-    children, modal
-  )
-}
-
-export function Parent() {
-
-  const [open, setOpen] = useState(false);
-
+export function Modal({ open, setOpen, children }) {
   return(
-    <div>
-      <svg onClick={() => setOpen(true)} height="200" viewBox="0 0 200 200" width="200">
-        <title />
-        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+    <div className={`overlay animated ${open ? 'show' : ''}`}>
+      <div className='modal'>
+        <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+          <title />
+          <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
         </svg>
-
-      {open && 
-        <Modal >
-          <div className='modal-wrapper'>
-            <div className='modal'>
-              Modal is opened !!!
-            </div>
-          </div>
-        </Modal>}
-    </div>
+        {children}
+      </div>
+  </div>
   )
 }
-
-
-
-
-// export function Modal({ open, setOpen, children }) {
-//   return(
-//     <div className={`overlay animated ${open ? 'show' : ''}`}>
-//       <div className='modal'>
-//         <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-//           <title />
-//           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-//         </svg>
-//         {children}
-//       </div>
-//   </div>
-//   )
-// }
