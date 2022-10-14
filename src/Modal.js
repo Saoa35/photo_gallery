601b5a592517a0ed514f5857e1f16c2e6ecab08e
Saoa35 <saoamah@gmail.com>
2022-10-14 21:48:28 +0200

    putted modal in collection div

--- src/Modal.js
+++ src/Modal.js
@@ -5,12 +5,11 @@ export function Modal({ open, setOpen, children }) {
   return(
     // <div className={`overlay animated ${open ? 'show' : ''}`}>
       <div className='modal'>
-        <svg  height="200" viewBox="0 0 200 200" width="200">
-        {/* onClick={() => setOpen(false)} */}
+        <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
           <title />
           <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
         </svg>
-        {children}
+        {/* {children} */}
       </div>
   // </div>
   )
